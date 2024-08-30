79aba13822265d103130ffd54abd984a5371be53
Stenio Wagner <stenio.wagner1@gmail.com>
2024-08-30 02:36:28 -0300

    feat: creating custom-errors

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,4 +4,11 @@ export const constants = {
   ADVANCE_X_STATE_UPDATES: 1,
   BOARD_CELL_SIZE: 8,
   BOARD_TEMPLATE_COLUMNS_SIZE: "2rem",
+  errors: {
+    UPDATE_CELL_STATE: "Failed to update cell state.",
+    UPDATE_NEXT_STATE: "Failed to update to next state.",
+    ADVANCE_X_STATE_UPDATES: "Failed to advance state updates.",
+    GENERATE_INITIAL_BOARD: "Failed to generate the initial board.",
+    PLAY_FOREVER: "Failed to play forever.",
+  },
 };
