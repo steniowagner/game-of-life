44acccce93e2d78ed3e1ba404ec916bb904c3123
Stenio Wagner <stenio.wagner1@gmail.com>
2024-08-31 00:20:07 -0300

    chore: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+// import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/game-of-life/",
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  // plugins: [react(), tsconfigPaths()],
+
   test: {
     globals: true,
     environment: "jsdom",
