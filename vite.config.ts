7e2fee5e52252b6a5a91ef65002b94b7633c5fd2
Stenio Wagner <stenio.wagner1@gmail.com>
2024-08-31 00:34:52 -0300

    chore: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     setupFiles: ["./src/test/setup.ts"],
   },
   resolve: {
