897c818c9b1aa5cdd0c218a9ebef78e23955e050
Stenio Wagner <stenio.wagner1@gmail.com>
2024-08-31 00:33:07 -0300

    chore: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": new URL("./src/", import.meta.url).pathname,
+      "@/": new URL("./src/", import.meta.url).pathname,
     },
   },
 });
